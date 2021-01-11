caec71723c5c986545d23612e6d1fb2b795edb5e
Kirill Kubryakov <kubryakov@gmail.com>
2021-01-11 20:57:59 +0300

    Keep styles with copying content, fix insterting headers on empty lines

--- scripts/editor.js
+++ scripts/editor.js
@@ -2,16 +2,64 @@
 
 const editArea = document.querySelector("div.edit-area");
 
+const tagStyles = {};
+
+const applyClassNameAndStyles = (node, className) => {
+  const style = tagStyles[node.nodeName];
+
+  if (style) {
+    node.className = className;
+    node.style = style;
+  } else {
+    const defaultStyleChunks = window.getComputedStyle(node).cssText.split(";");
+    node.className = className;
+    const computedStyle = window
+      .getComputedStyle(node)
+      .cssText.split(";")
+      .filter((chunk, i) => chunk !== defaultStyleChunks[i])
+      .join(";");
+    tagStyles[node.nodeName] = computedStyle;
+    node.style = computedStyle;
+  }
+};
+
 const toggleHeader = (level) => {
   const headerTag = `H${level}`;
   const className = `header${level}-text`;
-  const currentTag = document.getSelection().focusNode.parentNode.nodeName;
+
+  let focusNode = window.getSelection().focusNode;
+
+  // Return focus to editArea if it is not focused.
+  if (!focusNode || !editArea.contains(focusNode)) {
+    editArea.focus();
+    focusNode = window.getSelection().focusNode;
+  }
+
+  // Insert empty header when applied to to edit area itself or to an empty line.
+  if (focusNode.className === "edit-area" || focusNode.innerText === "\n") {
+    document.execCommand(
+      "insertHTML",
+      false,
+      // Style fix to enable cursor.
+      `<${headerTag} style="display:block;min-height:1px"></${headerTag}>`
+    );
+    applyClassNameAndStyles(window.getSelection().focusNode, className);
+
+    editArea.focus();
+    return;
+  }
+
+  const currentTag = focusNode.parentNode.nodeName;
 
   if (headerTag === currentTag) {
     document.execCommand("formatBlock", false, "DIV");
+    const newNode = window.getSelection().focusNode.parentNode;
+    newNode.className = undefined;
+    newNode.style = undefined;
   } else {
     document.execCommand("formatBlock", false, headerTag);
-    document.getSelection().focusNode.parentNode.className = className;
+    const newNode = window.getSelection().focusNode.parentNode;
+    applyClassNameAndStyles(newNode, className);
   }
 
   editArea.focus();
